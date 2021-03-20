c4b8569bb9dabe7ac71e9697c4f2af190d285494
Adrian <59733302+Grimm4iK@users.noreply.github.com>
2021-03-20 10:52:16 +0300

    Add files via upload

---
+++ gridnineproject/src/com/gridnine/testing/Main.java
@@ -0,0 +1,31 @@
+package com.gridnine.testing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        List<Flight> flights = FlightBuilder.createFlights();
+        ConsoleHelper.printFlights(flights);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        FlightFilter flightFilter = new TestingFlightFilter();
+        while(true) {
+            String input = br.readLine();
+            if (input.equals("1")) {
+                ConsoleHelper.printFlights(flightFilter.departureBeforeRequestedDateExclude(flights, LocalDateTime.now()));
+            }
+            if (input.equals("2")) {
+                ConsoleHelper.printFlights(flightFilter.arrivalBeforeDepartureExclude(flights));
+            }
+            if (input.equals("3")) {
+                ConsoleHelper.printFlights(flightFilter.groundTimeMoreThanTwoHoursExclude(flights));
+            } if (input.equals("4")) {
+                br.close();
+                return;
+            }
+        }
+    }
+}
